cd8a770a83cef71ec86aa1fca4901092afcc1045
dongganxixi <ruixing.xixi@gmail.com>
2024-06-30 21:55:32 +0800

    修改tab的设置

--- RickAndMorty/Controllers/Core/RMTabViewController.swift
+++ RickAndMorty/Controllers/Core/RMTabViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class RMTabViewController: UITabBarController {
-
+final class RMTabBarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setUpTabs()
     }
-
-
+    private func setUpTabs(){
+        let charactersVC = RMCharacterViewController()
+        let locationsVC = RMLocationrViewController()
+        let episodesVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        
+        let nav1 = UINavigationController(rootViewController: charactersVC)
+        let nav2 = UINavigationController(rootViewController: locationsVC)
+        let nav3 = UINavigationController(rootViewController: episodesVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        setViewControllers(
+            [nav1, nav2, nav3, nav4],
+            animated: true)
+        
+    }
+    
 }
 
